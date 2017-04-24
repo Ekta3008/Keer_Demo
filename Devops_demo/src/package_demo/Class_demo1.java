bf31a4b3bdc3c8c29124525a7bd3f7c5dadfa980
Ekta3008 <ep00348130@techmahindra.com>
2017-04-24 16:01:17 +0530

    Version 2 updated-ekta

---
+++ Devops_demo/src/package_demo/Class_demo1.java
@@ -0,0 +1,11 @@
+package package_demo;
+
+public class Class_demo1 {
+
+	public static void main(String[] args) {
+		System.out.println("keerti version1");
+		System.out.println("keerti version2");
+
+	}
+
+}
